49a6e061b7ae02da5841d3022021d35cfa3de723
yanxiaowu <296064285@qq.com>
2021-01-12 23:59:12 +0800

    3.explain

--- 3.searchMatrix/searchMatrix.cpp
+++ 3.searchMatrix/searchMatrix.cpp
@@ -1,4 +1,5 @@
 bool searchMatrix(int** matrix, int matrixSize, int* matrixColSize, int target){
+    //从第一行最右向左向下查找，因为比它的都在它下面，比他小的都在它左边
     //matrixSize:行数
     //*matrixColSize:列数
     int top = 0, right = *matrixColSize - 1;
